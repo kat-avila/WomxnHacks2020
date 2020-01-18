109308cee6d3c5678904f74efcde913d69aaae5c
katia <kavilapi@ucsc.edu>
2020-01-18 15:01:59 -0800

    Pop up exit

--- app/src/main/java/com/example/myapplicationtest1/MainActivity.java
+++ app/src/main/java/com/example/myapplicationtest1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplicationtest1;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,12 +12,15 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import static android.app.PendingIntent.getActivity;
 
 public class MainActivity extends AppCompatActivity {
     //TextView questionText;
@@ -32,26 +36,32 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         final TextView exitText = (TextView) findViewById(R.id.exitText);
+        final Button yesBtn = (Button) findViewById(R.id.yesBtn);
         final FloatingActionButton exitBtn = findViewById(R.id.exitBtn);
         exitBtn.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
                 exitText.setVisibility(View.VISIBLE);
-            }
-        });
+                yesBtn.setVisibility(View.VISIBLE);
+                yesBtn.setOnClickListener(new View.OnClickListener() {           // Yes Button
+                    @Override
+                    public void onClick(View v) {
+                        //finishAffinity();
+                        System.exit(0);
+                        //finish();
+                        //finishAndRemoveTask();
+                    }
+                });
+                }
+            });
+
 
         //this is me trying to do random question functionality
         //questions = {getString(R.string.question1), getString(R.string.question2), getString(R.string.question3)};
        // answers = {Integer.parseInt(getString(R.string.question1Ans)), Integer.parseInt(getString(R.string.question2Ans)), Integer.parseInt(getString(R.string.question3Ans))};
-
-
         //questionText = (TextView) findViewById(R.id.question);
 
 
-        final EditText editText = (EditText) findViewById(R.id.editText);
-        final Button submitBtn = (Button) findViewById(R.id.submitButton);
-        final TextView question = (TextView) findViewById(R.id.question);
         final ConstraintLayout bg = (ConstraintLayout) findViewById(R.id.layoutBg);
         //#B97299 , #DBD0D9 PINKS
         //#72B9B9, #D0DBDB  BLUES
@@ -60,6 +70,10 @@ public class MainActivity extends AppCompatActivity {
         bg.setBackgroundColor(0xFFD0DBDB);
         toolbar.setBackgroundColor(0xFF72B9B9);
 
+
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        final Button submitBtn = (Button) findViewById(R.id.submitButton);
+        final TextView question = (TextView) findViewById(R.id.question);
         final Switch sw = (Switch) findViewById(R.id.switch1);
         sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
